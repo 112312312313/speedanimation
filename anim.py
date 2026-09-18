e179e41714d9ce09e550ee6144ff36510d98746b
Danil <hfjdfud51@gmail.com>
2026-09-18 23:05:11 +0300

    source code, lol

---
+++ anim.py
@@ -0,0 +1,15 @@
+import time, sys
+
+def slowan(text, delay):
+    "The first parameter is the text itself, and the second is the typing speed; example: `slowan('hello,world!', 0.5)`."
+    if isinstance(text,int):
+            print("[ERROR] You cannot enter numbers without ''!")
+            sys.exit(1)
+    else:
+         pass
+    for c in str(text):
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(delay)
+        
+
